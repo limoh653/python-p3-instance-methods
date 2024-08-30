5a55a49057a2f7c7ce655adf2c3fb3953f676966
limoh653 <limohesbon7@gmail.com>
2024-08-30 08:03:43 +0300

    all tests passing

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 class Person:
+    def talk(self):
+        print("Hello World!")
+    def walk(self):
+        print("The person is walking.")
+
     # Class body goes here
 
     #Instance method definition
-    pass
+Guido= Person()
+Guido.talk
+Guido.walk
+pass
